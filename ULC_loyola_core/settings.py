a81869418e3b39879604ac4bd0e6e1188aff7a3b
robertakany <robertakany2@gmail.com>
2023-10-17 14:38:50 +0100

    update

--- ULC_loyola_core/settings.py
+++ ULC_loyola_core/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
+STATICFILES_DIRS = [BASE_DIR.joinpath('staticfiles/')]
+STATIC_ROOT = BASE_DIR.joinpath('static/')
 
 
 MEDIA_URL = '/media/'
