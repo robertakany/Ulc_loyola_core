32c04d734d20b75da26d610aa27f8843ac0cf975
robertakany <robertakany2@gmail.com>
2023-11-10 18:40:14 +0100

    add the possibility to upload a lot of images and edit the models

--- ULC_loyola_core/settings.py
+++ ULC_loyola_core/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "loyola_db",
         "USER": "postgres",
-        "PASSWORD": "aphrodite",
+        "PASSWORD": "derferger",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
