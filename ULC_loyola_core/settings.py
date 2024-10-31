9906caa17adb05ace9b13c205b643ceab6fd745f
robert akany <robertakany2@gmail>
2024-10-31 12:52:18 +0100

    upgrade

--- ULC_loyola_core/settings.py
+++ ULC_loyola_core/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',  
 ]
 
 ROOT_URLCONF = 'ULC_loyola_core.urls'
