2b4390ad87859a8e9d80a69bedfd5bae132728bb
robert akany <robertakany2@gmail>
2025-03-10 13:24:06 +0100

    upgrade

--- main/utils.py
+++ main/utils.py
@@ -1,7 +1,7 @@
 Faculty = (
     ('Philosophe','philosophie'),
-	('Faculté des Sciences et Technologies','Faculté des Sciences et Technologies'),
+	('Sciences et Technologies','Sciences et Technologies'),
 	('Agronomiques et Vetérinaires', 'Agronomiques et Vetérinaires'),
 	('Sciences Sociales et gestion','Sciences Sociales et gestion'),
- 	('Administration des Affaires - Business school','Administration des Affaires - Business school'),
+ 	("Administration des Affaires - Business school","Administration des Affaires - Business school"),
 )
